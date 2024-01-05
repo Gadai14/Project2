1ec89aa55d526a82847f75a62ebe8f0793de5f59
Abhijit Pal <121002242+Gadai14@users.noreply.github.com>
2024-01-05 18:01:12 +0530

    Add files via upload

---
+++ NEW/que5.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let que5 = [
+    {
+      numb: 1,
+      question: "Who invented Java Programming?",
+      answer: "James Gosling",
+      options: [
+        "Guido van Rossum",
+        "James Gosling",
+        "Bjarne Stroustrup",
+        "Dennis Ritchie"
+        
+      ]
+    },
+      
+   
+      {
+      numb: 2,
+      question: "Which one of the following is not a Java feature?",
+      answer: "Use of pointers",
+      options: [
+        "Object-oriented",
+        "Use of pointers",
+        "Portable",
+        "Dynamic and Extensible"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Which of the following is not an OOPS concept in Java?",
+      answer: "Compilation",
+      options: [
+        "Polymorphism",
+        "Inheritance",
+        "Compilation",
+        "Encapsulation"
+      ]
+    },
+    {
+      numb: 4,
+      question: "Which exception is thrown when java is out of memory?",
+      answer: "OutOfMemoryError",
+      options: [
+        "MemoryError",
+        "OutOfMemoryError",
+        "MemoryOutOfBoundsException",
+        "MemoryFullException"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which of these packages contains the exception Stack Overflow in Java?",
+      answer: "java.lang",
+      options: [
+        "java.io",
+        "java.system",
+        "java.lang",
+        "java.util"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Which of these keywords are used for the block to be examined for exceptions?",
+      answer: "try",
+      options: [
+        "check",
+        "throw",
+        "catch",
+        "try"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Which one of the following is not an access modifier?",
+      answer: "Void",
+      options: [
+        "Protected",
+        "Void",
+        "Public",
+        "Private"
+      ]
+    },
+    {
+      numb: 8,
+      question: " Which of the following is a reserved keyword in Java?",
+      answer: "strictfp",
+      options: [
+        "object",
+        "strictfp",
+        "main",
+        "system"
+      ]
+    },
+    {
+      numb: 9,
+      question: "In java, jar stands for -",
+      answer: "None of the above",
+      options: [
+        "Java Archive Runner",
+        "Java Application Resource",
+        "Java Application Runner",
+        "None of the above"
+      ]
+    },
+    {
+      numb: 10,
+      question: "How many threads can be executed at a time? ",
+      answer: "Multiple threads",
+      options: [
+        "Only one thread",
+        "Multiple threads",
+        "Only main (main() method) thread",
+        "Two threads"
+      ]
+    }
+      
+  ];
