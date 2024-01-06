1ea4fcffab520ee75e2d7fcc221eeef73e777b85
Abhijit Pal <121002242+Gadai14@users.noreply.github.com>
2024-01-06 08:24:24 +0530

    Add files via upload

---
+++ NEW/que2.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let que2 = [
+    {
+      numb: 1,
+      question: "CSS stands for -",
+      answer: "Cascading style sheets",
+      options: [
+        "Cascade style sheets",
+        "Color and style sheets",
+        "Cascading style sheets",
+        "None of the above"
+      ]
+    },
+      {
+      numb: 2,
+      question: "Which if the following CSS function allows us to perform calculations?",
+      answer: "calc() function",
+      options: [
+        "calc() function",
+        "calculator() function",
+        "calculate() function",
+        "cal() function"
+      ]
+    },
+   
+    {
+      numb: 3,
+      question: " How can you add a hover effect to an element in CSS?",
+      answer: ":hover",
+      options: [
+        ".hover",
+        "#hover",
+        "hover: true;",
+        ":hover"
+      ]
+    },
+    {
+      numb: 4,
+      question: "What is the default value of the display property in CSS?",
+      answer: "block",
+      options: [
+        "inline",
+        "block",
+        "offline",
+        "None of these"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which css property is used to specify transparency of an element?",
+      answer: "opacity",
+      options: [
+        "filter",
+        "visibility",
+        "filter",
+        "opacity"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Which CSS property is used to control the spacing between lines of text?",
+      answer: " line-height ",
+      options: [
+        "letter-spacing ",
+        "text-spacing ",
+        " line-height ",
+        "text-line-height "
+      ]
+    },
+    {
+      numb: 7,
+      question: "Which property is used to create rounded corners for an element?",
+      answer: "border-radius ",
+      options: [
+        "border-style ",
+        "border-radius ",
+        "border-width ",
+        "border-color"
+      ]
+    },
+    {
+      numb: 8,
+      question: "What is the default position value of an element in CSS?",
+      answer: "static",
+      options: [
+        "static",
+        "relative",
+        "absolute",
+        "fixed"
+      ]
+    },
+    {
+      numb: 9,
+      question: "Which property is used to change the text size of an element?",
+      answer: "font-size ",
+      options: [
+        " text-size ",
+        "font-size ",
+        "text-style ",
+        "size "
+      ]
+    },
+    {
+      numb: 10,
+      question: "Which property is used to add a background image?",
+      answer: "background-image",
+      options: [
+        "background-color ",
+        "color ",
+        "background-image",
+        "image-background "
+      ]
+    },
+      
+  ];
---
+++ NEW/que6.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let que6 = [
+    {
+      numb: 1,
+      question: "Which package manager is bundled with Node.js by default?",
+      answer: "npm",
+      options: [
+        "npm",
+        "bower",
+        "yarn",
+        "grunt"
+        
+      ]
+    },
+      
+   
+      {
+      numb: 2,
+      question: "Which method is used in Node.js to include modules?",
+      answer: "require()",
+      options: [
+        "require()",
+        "include()",
+        "import",
+        "fetch()"
+      ]
+    },
+    {
+      numb: 3,
+      question: "In Node.js, which of the following is an in-built event emitter?",
+      answer: "EventEmitter",
+      options: [
+        "EventEmitter",
+        "Events",
+        "EventLoop",
+        "EventYield"
+      ]
+    },
+    {
+      numb: 4,
+      question: "How can you read command-line arguments in a Node.js application?",
+      answer: "process.argv",
+      options: [
+        "argv",
+        "process.args",
+        "arguments",
+        "process.argv"
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which of the following is a core module for handling paths?",
+      answer: "path",
+      options: [
+        "url",
+        "dir",
+        "path",
+        "location"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Which of the following allows Node.js to be scalable?",
+      answer: "Event-driven architecture",
+      options: [
+        "Multithreading",
+        "Event-driven architecture",
+        "Larger memory allocation",
+        "High CPU usage"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Node.js uses _ programming",
+      answer: "asynchronous",
+      options: [
+        "asynchronous",
+        "synchronous",
+        "Both A and B",
+        "None of the above"
+      ]
+    },
+    {
+      numb: 8,
+      question: "What does the fs module stand for?",
+      answer: "File System",
+      options: [
+        "File Service",
+        "File System",
+        "File Store",
+        "File Sharing"
+      ]
+    },
+    {
+      numb: 9,
+      question: "Which of the following template engines can be used with Node.js?",
+      answer: "Handlebars",
+      options: [
+        "Jade",
+        "Vash",
+        "Handlebars",
+        "All of the above"
+      ]
+    },
+    {
+      numb: 10,
+      question: "What is the default scope in the Node.js application?",
+      answer: "Local",
+      options: [
+        "Global",
+        "Local",
+        "Global Function",
+        "Local to object"
+      ]
+    }
+      
+  ];
